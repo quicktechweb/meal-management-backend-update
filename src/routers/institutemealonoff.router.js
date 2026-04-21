54664778ec1354517a7c34b8d3e7f400342c81bc
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2026-04-21 17:15:22 +0600

    institute meal on off time get api added

--- src/routers/institutemealonoff.router.js
+++ src/routers/institutemealonoff.router.js
@@ -7,16 +7,44 @@ const instituteRequireAuth = require("../middlewares/instituteAuth.middleware");
 
 router.post("/meal-on-off-time", instituteRequireAuth, async (req, res) => {
   try {
-    const { institute_id, meal_on_off_time } = req.body;
+    const { meal_on_off_time } = req.body;
+    const institute_id = req.user._id;
 
-    const data = await Institutemealonofftime.create({
-      institute_id,
-      meal_on_off_time,
+    if (!meal_on_off_time) {
+      return res.status(400).json({
+        success: false,
+        message: "meal_on_off_time is required",
+      });
+    }
+
+    const data = await Institutemealonofftime.findOneAndUpdate(
+      { institute_id },
+      { $set: { meal_on_off_time } },
+      { upsert: true, new: true, runValidators: true },
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Saved Successfully",
+      data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+router.get("/meal-on-off-time", instituteRequireAuth, async (req, res) => {
+  const user = req.user;
+
+  try {
+    const data = await Institutemealonofftime.findOne({
+      institute_id: user.institute_id,
     });
 
-    res.status(201).json({
+    res.json({
       success: true,
-      message: "Created Successfully",
       data,
     });
   } catch (error) {
@@ -28,44 +56,19 @@ router.post("/meal-on-off-time", instituteRequireAuth, async (req, res) => {
 });
 
 router.get(
-  "/meal-on-off-time/:institute_id",
+  "/institute-meal-on-off-time",
   instituteRequireAuth,
   async (req, res) => {
+    const user = req.user;
+
     try {
       const data = await Institutemealonofftime.findOne({
-        institute_id: req.params.institute_id,
+        institute_id: user._id,
       });
 
       res.json({
         success: true,
         data,
-      });
-    } catch (error) {
-      res.status(500).json({
-        success: false,
-        message: error.message,
-      });
-    }
-  },
-);
-
-router.put(
-  "/meal-on-off-time/:institute_id",
-  instituteRequireAuth,
-  async (req, res) => {
-    try {
-      const { meal_on_off_time } = req.body;
-
-      const updated = await Institutemealonofftime.findOneAndUpdate(
-        { institute_id: req.params.institute_id },
-        { meal_on_off_time },
-        { new: true, upsert: true },
-      );
-
-      res.json({
-        success: true,
-        message: "Updated Successfully",
-        data: updated,
       });
     } catch (error) {
       res.status(500).json({
